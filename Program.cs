dec5e591867e2710f63d6ef2b0701509a1f8acec
developer2 <developer2@DEVELOPER2>
2016-10-25 16:52:09 +0300

    start

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupSystem
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Выберите режим работы:");
+            Console.WriteLine("1 - режим наблюдения");
+            Console.WriteLine("2 - режим отката изменений");
+
+            int mode;
+            while (!int.TryParse(Console.ReadLine(), out mode))
+            {
+                Console.WriteLine("Неизвестный параметр режима работы");
+            }
+            
+            if (mode == 1)
+            {
+                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine("Программа работает в режиме наблюдения...");
+                Watcher.Start();
+            }
+            else
+            {
+                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine("Производится откат изменений...");
+            }
+        }
+    }
+}
---
+++ Watcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupSystem
+{
+    public class Watcher
+    {
+        private static FileSystemWatcher watcher;
+
+        public static void Start()
+        {
+            watcher = new FileSystemWatcher(@"D:\New");
+            watcher.IncludeSubdirectories = true;
+            watcher.Created += OnCreated;
+        }
+
+        public static void Stop()
+        {
+
+        }
+
+        private static void OnCreated(object obj, EventArgs e)
+        {
+            
+        }
+    }
+}
